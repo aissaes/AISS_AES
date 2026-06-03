eb1ea915ed5bec92c1182803dbc4251e1f69f651
Vana Karthik <vkarthik4321@gmail.com>
2026-06-03 19:48:51 +0530

    feat: add ScrollToTop component to reset scroll position on route change

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,10 +4,12 @@ import { ToastProvider } from './components/Toast/Toast';
 import { AuthProvider } from './context/AuthContext';
 import { ThemeProvider } from './context/ThemeContext';
 import AppRoutes from './routes/AppRoutes';
+import ScrollToTop from './components/ScrollToTop/ScrollToTop';
 
 function App() {
   return (
     <BrowserRouter>
+      <ScrollToTop />
       <ThemeProvider>
         <ToastProvider>
           <AuthProvider>
